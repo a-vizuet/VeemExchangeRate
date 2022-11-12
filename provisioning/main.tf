f71f4794bf1e696884b810c42ca486a4a884fd54
megustalafantabienfria <26828033+megustalafantabienfria@users.noreply.github.com>
2022-11-12 17:26:31 -0600

    Gitignore and main terraform updated

--- provisioning/main.tf
+++ provisioning/main.tf
@@ -1,3 +1,6 @@
+resource "random_id" "random_path" {
+	byte_length = 16
+}
 resource "google_secret_manager_secret" "telegram-token" {
   secret_id = "telegram_token"
 
@@ -24,4 +27,12 @@ resource "google_secret_manager_secret_version" "veem_token_value" {
   secret = google_secret_manager_secret.veem-token.id
 
   secret_data = var.veem-token
+}
+
+resource "google_cloudfunctions_function" "handleEvents" {
+  secret_environment_variables {
+    key = "telegram-token"
+    secret = google_secret_manager_secret.telegram-token.secret_id
+    version = google_secret_manager_secret_version.veem_token_value.version
+  }
 }
